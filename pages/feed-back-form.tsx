ef5bd336da39284b7132674fe2e1d5bcaa06b456
Shreeyanch Shrestha <shsh19cs@cmrit.ac.in>
2024-05-22 22:25:21 +0545

    first commit

---
+++ pages/feed-back-form.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from "next";
+import { useCallback } from "react";
+import Form from "../components/form";
+import styles from "./feed-back-form.module.css";
+
+const FeedBackForm: NextPageFeedBackFormType = () => {
+  const onLETTERLOGO2Click = useCallback(() => {
+    window.open("https://www.samparka.info/");
+  }, []);
+
+  return (
+    <div className={styles.feedbackform}>
+      <div className={styles.shareYourTravelsForm}>
+        <div className={styles.formHeader}>
+          <img className={styles.image12Icon} alt="" src="/image-12@2x.png" />
+        </div>
+        <Form />
+      </div>
+      <div className={styles.feedbackformInner}>
+        <div className={styles.frameWrapper}>
+          <div className={styles.poweredByParent}>
+            <div className={styles.poweredBy}>powered by</div>
+            <img
+              className={styles.letterLogo2}
+              alt=""
+              src="/letter-logo-2@2x.png"
+              onClick={onLETTERLOGO2Click}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedBackForm;
